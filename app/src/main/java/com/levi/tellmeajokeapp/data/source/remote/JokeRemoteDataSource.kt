c380e55f8c078eb1a5bc3629581a6345556fcad9
leviNunez <81485281+leviNunez@users.noreply.github.com>
2023-03-14 17:19:24 -0400

    restored stashed uncommited changes

---
+++ app/src/main/java/com/levi/tellmeajokeapp/data/source/remote/JokeRemoteDataSource.kt
@@ -0,0 +1,20 @@
+package com.levi.tellmeajokeapp.data.source.remote
+
+import com.levi.tellmeajokeapp.data.Joke
+import com.levi.tellmeajokeapp.data.JokeApi
+import com.levi.tellmeajokeapp.data.Result
+import com.levi.tellmeajokeapp.data.Result.Success
+import com.levi.tellmeajokeapp.data.Result.Error
+import com.levi.tellmeajokeapp.data.source.JokeDataSource
+
+class JokeRemoteDataSource(private val api: JokeApi) : JokeDataSource {
+
+    override suspend fun getJoke(): Result<Joke> {
+        return try {
+            val joke = api.getRandomJoke()
+            Success(data = joke)
+        } catch (e: Exception) {
+            Error(e)
+        }
+    }
+}
